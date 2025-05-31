6c3a3eb0019c2e1a1f9f71ee4151fb305454f6c3
Achrafsadeq <achrafsadeqq@gmail.com>
2025-05-31 22:00:15 +0000

    Add session auth: endpoints, login/logout, cookies, expiration, DB sessions; update User and app logic

---
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+""" Session database authentication module
+"""
+from api.v1.auth.session_exp_auth import SessionExpAuth
+from models.user_session import UserSession
+from datetime import datetime, timedelta
+
+
+class SessionDBAuth(SessionExpAuth):
+    """ Session database authentication class
+    """
+    def create_session(self, user_id: str = None) -> str:
+        """ Creates a Session ID for a user_id
+        """
+        session_id = super().create_session(user_id)
+        if session_id is None:
+            return None
+        user_session = UserSession(user_id=user_id, session_id=session_id)
+        user_session.save()
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ Returns a User ID based on a Session ID
+        """
+        if session_id is None:
+            return None
+        UserSession.load_from_file()
+        user_sessions = UserSession.search({'session_id': session_id})
+        if not user_sessions:
+            return None
+        user_session = user_sessions[0]
+        if self.session_duration <= 0:
+            return user_session.user_id
+        if (user_session.created_at +
+                timedelta(seconds=self.session_duration) < datetime.now()):
+            return None
+        return user_session.user_id
+
+    def destroy_session(self, request=None):
+        """ Destroys the UserSession based on the Session ID
+        """
+        if request is None:
+            return False
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return False
+        user_sessions = UserSession.search({'session_id': session_id})
+        if not user_sessions:
+            return False
+        user_session = user_sessions[0]
+        user_session.remove()
+        return True
